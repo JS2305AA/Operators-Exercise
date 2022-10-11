e6272698299ac62f4810b857b0a35325d4945bac
2305 <npjt2305@yahoo.com>
2022-10-11 17:03:11 -0400

    completed exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+
+            int b = 4;
+
+            int quotient = a / b;
+
+            int remainder = a % b;
+
+           
+            
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            
+            
+          
+            
+
         }
+
+        public static double AreaOfCircle(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+
     }
 }
